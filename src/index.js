00ee215dc8850046f0a4c073f57906415cf8d04b
eubrunodev <bruno.peniani03@gmail.com>
2022-08-17 11:40:49 -0300

    First Up

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+require('dotenv').config()
+const PORT = process.env.PORT
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.send('Ok');
+})
+
+
+require('./app/controllers/index')(app);
+
+app.listen(PORT, () => {
+    console.log('Estou rodando na port '+PORT);
+});
